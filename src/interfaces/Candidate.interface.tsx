940903395af294db7841d4294700aebf42741374
kaneganteng <kaneesasta@gmail.com>
2024-11-02 22:03:39 -0400

    made an interface for Candidate in candidate.interface.tsx

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,10 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+export interface Candidate {
+    readonly name: string | null;
+    readonly username: string | null;
+    readonly location: string | null;
+    readonly avatar: string | null;
+    readonly email: string | null;
+    readonly html_url: string | null;
+    readonly company: string | null;
+}
